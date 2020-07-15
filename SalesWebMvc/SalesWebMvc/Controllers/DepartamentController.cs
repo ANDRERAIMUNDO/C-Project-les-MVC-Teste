25012a56c061c59ab59438220221c4283861b4e4
ANDRERAIMUNDO <9000andre@gmail.com>
2020-07-15 14:55:13 -0300

    Fisrt Model-Controller-View - Departament

---
+++ SalesWebMvc/SalesWebMvc/Controllers/DepartamentController.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using SalesWebMvc.Models;
+
+namespace SalesWebMvc.Controllers
+{
+    public class DepartamentController : Controller
+    {
+        public IActionResult Index()
+        {
+            List<Departament> list = new List<Departament>();
+            list.Add(new Departament { Id = 1, name = "Eletronics" });
+            list.Add(new Departament { Id = 2, name = "Fashion" });
+
+            return View(list);
+        }
+    }
+}
