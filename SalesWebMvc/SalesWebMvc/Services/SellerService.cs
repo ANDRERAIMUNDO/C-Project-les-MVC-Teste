42a242de5ce7a72280c83470d0bf2a3283df50b7
ANDRERAIMUNDO <9000andre@gmail.com>
2020-07-23 10:45:45 -0300

    Simple Create form whith erro in save Base

--- SalesWebMvc/SalesWebMvc/Services/SellerService.cs
+++ SalesWebMvc/SalesWebMvc/Services/SellerService.cs
@@ -10,13 +10,19 @@ namespace SalesWebMvc.Services
     public class SellerService
     {
         private readonly SalesWebMvcContext _context;
-        public SellerService (SalesWebMvcContext context)
+        public SellerService(SalesWebMvcContext context)
         {
             _context = context;
         }
         public List <Seller> FindAll()
         {
             return _context.Seller.ToList();
+        }
+
+        public void  Insert(Seller obj)
+        {
+            _context.Add(obj);
+            _context.SaveChanges();
         }
     }
 }
